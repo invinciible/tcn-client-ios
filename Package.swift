91567173e53ec9ad07c2d56a0f07d42dcb4c8150
Tushar Katyal <tktusharkatyal11@gmail.com>
2021-10-05 11:20:15 +0530

    Update Package.swift
    Set minimum iOS version to 12.0

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TCNClient",
     platforms: [
-        // TODO: Add support for iOS 12.
-      .iOS(.v13),
+      .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
